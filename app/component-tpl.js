8c83489834a612226cee8728b910efd7a1a9d9f8
Pavlo_Oliinyk1 <Pavlo_Oliinyk1@epam.com>
2018-04-13 12:45:18 +0300

    add style, add forceUpdateHandler

--- app/component-tpl.js
+++ app/component-tpl.js
@@ -2,11 +2,14 @@ import React from 'react';
 var createReactClass = require('create-react-class');
 //to use React.createClass: npm install create-react-class --save
 
+let rBool = Math.random() >= 0.5;
+
 const FirstComponent0 = createReactClass({
     render: function() {
         return (
             <React.Fragment>
                 <div>
+                    --createReactClass--
                     <p>Hello big Fragment</p>
                 </div>
             </React.Fragment>
@@ -35,20 +38,21 @@ const ButtonFnComponent = props => {
 
 const elementMain = (
     <h1 className="greeting">
-        some, element!        <span>It is {new Date().toLocaleTimeString()}.</span>
+        some --simple--, element!        <span>It is {new Date().toLocaleTimeString()}.</span>
     </h1>
 );
 
 const secondelement = React.createElement(
-    'h1',
-    {className: 'greeting'},
-    Math.random() >= 0.5 ? 'Hi, second element!' : 'Hey second element!'
+    'h2',
+    {className: 'greeting2'},
+    rBool ? 'Hi, second --simple-- element!' : 'Hey second element! --React.createElement--'
 );
 
 class PureComponent extends React.PureComponent {
     render() {
         return (
             <div>
+                <b>React.PureComponent</b>
                 <p>
                     Furthermore, React.PureComponent’s shouldComponentUpdate() skips prop updates for the whole component subtree. Make sure all the children components are also “pure”
                 </p>
@@ -59,16 +63,27 @@ class PureComponent extends React.PureComponent {
 }
 
 class FirstComponent extends React.Component {
+    constructor(){
+        super();
+        this.forceUpdateHandler = this.onClickButtonFnComponent.bind(this);
+    };
+
+    onClickButtonFnComponent(){
+        rBool = Math.random() >= 0.5;
+        console.log(rBool);
+        this.forceUpdate();
+    };
+
     render() {
         return (
             <div>
+                ***--React.Component-- simple text
                 {elementMain}
+                {(rBool) ? secondelement : secondelement /*???*/}
                 <FirstComponent0></FirstComponent0>
-                {secondelement}
-                ***simple text
                 <PureComponent></PureComponent>
-                <HelloFnComponent name='real Name goes here'></HelloFnComponent>
-                <ButtonFnComponent>Button styling Fn Component</ButtonFnComponent>
+                <HelloFnComponent name='real Name goes here. --HelloFnComponent--'></HelloFnComponent>
+                <ButtonFnComponent onClick={this.forceUpdateHandler}>Button styling --Fn Component--</ButtonFnComponent>
             </div>
         )
     }
